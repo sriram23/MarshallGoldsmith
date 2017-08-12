f405039035ea0a7a367666d21ed441c678e3936b
sriram23 <sriramsubramanian23@gmail.com>
2017-08-12 13:25:17 +0530

    Tried to add shared preference. But failed.

--- app/src/main/java/com/example/sriram/marshallgoldsmith/MainActivity.java
+++ app/src/main/java/com/example/sriram/marshallgoldsmith/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.sriram.marshallgoldsmith;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usrnam;
+    TextView namtxt;
+    Button namBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usrnam = (EditText)findViewById(R.id.yourNameET);
+        namtxt = (TextView)findViewById(R.id.yourName);
+        namBtn = (Button)findViewById(R.id.btn_yourName);
     }
 }
