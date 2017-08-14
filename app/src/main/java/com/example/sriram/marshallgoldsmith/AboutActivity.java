e56f7c280b98af2e2e64c8ff7c610e7cc101218f
sriram23 <sriramsubramanian23@gmail.com>
2017-08-14 17:12:21 +0530

    Added Some styles and About section

---
+++ app/src/main/java/com/example/sriram/marshallgoldsmith/AboutActivity.java
@@ -0,0 +1,43 @@
+package com.example.sriram.marshallgoldsmith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        TextView helloTxt = (TextView)findViewById(R.id.hellotxt);
+        helloTxt.setText(readTxt());
+    }
+
+    private String readTxt(){
+
+        InputStream inputStream = getResources().openRawResource(R.raw.marsh);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return byteArrayOutputStream.toString();
+    }
+}
--- app/src/main/java/com/example/sriram/marshallgoldsmith/WelcomeActivity.java
+++ app/src/main/java/com/example/sriram/marshallgoldsmith/WelcomeActivity.java
@@ -1,17 +1,23 @@
 package com.example.sriram.marshallgoldsmith;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class WelcomeActivity extends AppCompatActivity {
     TextView tx;
+    Button Abt;
+    Button Tk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,15 @@ public class WelcomeActivity extends AppCompatActivity {
         Log.d("TAG", value);
         tx = (TextView) findViewById(R.id.Name_New);
         tx.setText("Hello..!"+value);
+        Abt = (Button) findViewById(R.id.abt);
+        Tk = (Button) findViewById(R.id.TT);
+
+        Abt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(WelcomeActivity.this, AboutActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
